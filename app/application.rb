3e3a0caa8feb2170c56b6fc342caa9332a57c6c9
warrenniu <warrenniu@gmail.com>
2020-10-25 12:11:16 -0400

    Completed

---
+++ app/application.rb
@@ -0,0 +1,29 @@
+class Application
+    def call(env)
+        resp = Rack::Response.new
+        time = Time.now
+
+        if time.hour >= 12
+            # binding.pry
+            resp.write "Good Afternoon"
+        else
+            resp.write "Good Morning"
+        end
+
+        resp.finish
+    end
+end
+
+# class Application
+
+#     def call(env)
+#       resp = Rack::Response.new
+#       now = Time.now
+#       if now.hour >=12
+#         resp.write "Good Afternoon"
+#       else
+#         resp.write "Good Morning"
+#       end
+#       resp.finish
+#     end
+  
